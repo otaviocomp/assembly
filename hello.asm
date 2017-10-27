399c33324ad6667b61750508eb87a42973691246
otaviocomp <otavio10espsanto@gmail.com>
2017-10-27 05:37:45 -0300

    hello world

--- hello.asm
+++ hello.asm
@@ -2,15 +2,15 @@ section.text
 global_start
 
 _start:
-	mov edx, len
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
-	int 0x80
+	mov edx, len					;message len
+	mov ecx, msg					;message to write 
+	mov ebx, 1						;file descriptor(stdout)
+	mov eax, 4						;system call number (sys_write)
+	int 0x80						;call kernel
 
-	mov eax, 1
-	int 0x80
+	mov eax, 1						;system call number(sys_exit)
+	int 0x80						;call kernel
 
 section.data
-	msg db 'Hello Wolrld!', 0xa
-	len equ $ -msg 
+	msg db 'Hello Wolrld!', 0xa		;string to be printed
+	len equ $ -msg					;length of the string
